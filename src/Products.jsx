5e58477d005d905b04bd8c69eb5bf21758658c49
FullofMyself <wmaseko1@live.com>
2025-07-02 16:50:56 +0200

    Fix images

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,14 +5,14 @@ const products = [
     id: 1,
     name: "Tassel 12 Hour Concentrated Skin Balm",
     price: 299,
-    image: "/images/products/Tassel_12_Hour_Concentrated_Skin_Balm.jpg",
+    image: `${import.meta.env.BASE_URL}images/products/Tassel_12_Hour_Concentrated_Skin_Balm.jpg`,
     description: "Nourish your skin with our signature spa facial cream.",
   },
   {
     id: 2,
     name: "Tassel Beard & Hair Oil",
     price: 199,
-    image: "/images/products/Tassel_Beard_&_Hair_Oil.jpg",
+    image: `${import.meta.env.BASE_URL}images/products/Tassel_Beard_&_Hair_Oil.jpg`,
     description: "Relax and unwind with our soothing aromatic massage oil.",
   },
   // Add more products as needed
