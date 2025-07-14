7ac96caf24e5fb55812f1f534c19708085f0ac30
FullofMyself <wmaseko1@live.com>
2025-07-14 16:35:21 +0200

    HeroSection Image

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -4,8 +4,13 @@ import "./styles.css";
 
 export default function HeroSection() {
   return (
-    <section className="hero-section">
-      <div className="hero-content">
+    <section className="hero-section-img">
+      <img
+        src="/public/images/products/Tassel_Retinol_A_&E_Night_Cream_Anti_Wrinkle.jpg"
+        alt="Tassel Beauty background"
+        className="hero-image"
+      />
+      <div className="hero-content-img">
         <h1 className="hero-title">Glow Starts Here</h1>
         <p className="hero-subtitle">Discover our natural skincare essentials.</p>
         <a href="#product-list" className="hero-cta">Shop Now</a>
