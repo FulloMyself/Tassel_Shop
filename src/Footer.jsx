6a4b3a78e9a9ddd7d603079a79563ee39049d56c
FullofMyself <wmaseko1@live.com>
2025-07-14 16:16:37 +0200

    Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,12 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <img
-        src={`${import.meta.env.BASE_URL}images/products/Tassel_LOGO.png`}
-        alt="Tassel Logo"
-        className="footer-logo"
-        style={{ height: 40, marginBottom: 8 }}
-      />
+      <a href="https://tasselgroup.co.za/">
+            <img src={`${import.meta.env.BASE_URL}images/products/Tassel_LOGO.png`} alt="Tassel Logo" className="logo" style={{ height: 40, marginBottom: 8 }}/>
+          </a>
       <p>&copy; {new Date().getFullYear()} Tassel Beauty And Wellness Studio</p>
     </footer>
   );
