fe05c956bdc4f71fdc3c6f692afdc154f46ebbfc
FullofMyself <wmaseko1@live.com>
2025-08-05 11:11:49 +0200

    Services Hours

--- src/Bookings.jsx
+++ src/Bookings.jsx
@@ -30,7 +30,7 @@ export default function Bookings() {
   }, []);
 
   const businessHours = {
-    Monday: { start: "09:00", end: "17:00"}
+    Monday: { start: "09:00", end: "17:00"},
     Tuesday: { start: "09:00", end: "17:00" },
     Wednesday: { start: "09:00", end: "17:00" },
     Thursday: { start: "09:00", end: "17:00" },
