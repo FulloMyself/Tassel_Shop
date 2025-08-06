a3abe64a6aee64abce2b8b0bcd03353241809ed3
FullofMyself <wmaseko1@live.com>
2025-08-06 10:25:53 +0200

    Services Slide

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import gsap from "gsap";
 import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./Header";
 import HeroSection from "./HeroSection";
